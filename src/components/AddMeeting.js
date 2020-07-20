252a7dcb982083a4fcc5317eed4a6d1804a63e1f
Ravi Bhaskar <bhaskarravi2000@gmail.com>
2020-07-20 14:17:46 +0530

    changes

--- src/components/AddMeeting.js
+++ src/components/AddMeeting.js
@@ -3,6 +3,8 @@ import {Breadcrumb,BreadcrumbItem, Input, Button,Form, FormGroup, Label} from 'r
 import {Link } from 'react-router-dom';
 import DatePicker from "react-datepicker";
 import TimePicker from 'react-time-picker';
+import {Row,Col} from 'reactstrap';
+import {Control} from 'react-redux-form';
 
 class AddMeeting extends Component{
     constructor(props){
@@ -13,22 +15,20 @@ class AddMeeting extends Component{
             meetingdate:null,
             description:null,
         }
-        this.onChangeMeeting=this.onChangeMeeting.bind(this);
-        this.onChangeStartTime=this.onChangeStartTime.bind(this);
-        this.onChangeEndTime=this.onChangeEndTime.bind(this);
-        this.onChangeDescription=this.onChangeDescription.bind(this);
-        this.handlesubmit=this.handlesubmit.bind(this);
+        
+        this.handleSubmit=this.handleSubmit.bind(this);
 
     }
-    onChangeMeeting = meetingdate => this.setState({ meetingdate:meetingdate});
-    onChangeStartTime=time => this.setState({ starttime:time});
-    onChangeEndTime=time => this.setState({endtime:time});
-    onChangeDescription=description=>this.setState({description:description});
-
-    handlesubmit(event){
-       alert('Slot not available')
-       event.preventDefault();
+    
+    handleSubmit(event){
+      
+        alert("Meeting date: " + this.meetingdate.value+" start time: " +this.starttime.value+  "End Time: "+ this.endtime.value+"Desriptions: " +this.description.value);
+        event.preventDefault();
+      
+       
+ 
     }
+ 
     render(){
         return(
                 <div className="container">
@@ -38,36 +38,63 @@ class AddMeeting extends Component{
                         </BreadcrumbItem>
                         <BreadcrumbItem active>Add Meetings</BreadcrumbItem>
                     </Breadcrumb>
-               
+                   
+                   
+                    <Form model="feedback" onSubmit={this.handleSubmit}>
+                            
+                    <Row className="form-group">
+                            <Label htmlFor="meeetingdate" md={2} >Meetings</Label>
+                            <Col md={3}>
+                                <Input type="date" className="form-control" id="meetingdate" name ="meetingdate"
+                                innerRef={(input)=>this.meetingdate=input} />
+                            
+                                
+                            </Col>
+                            
+                     </Row>
+
+                     <Row className="form-group">
+                            <Label htmlFor="starttime" md={2} >Start Time</Label>
+                            <Col md={3}>
+                                <Input type="time" className="form-control" id="starttime" name ="starttime"
+                                innerRef={(input)=>this.starttime=input} />
+                            
+                                
+                            </Col>
+                     
+                    </Row>
+
+                    <Row className="form-group">
+                    <Label htmlFor="endtime" md={2} >End Time</Label>
+                    <Col md={3}>
+                        <Input type="time" className="form-control" id="endtime" name ="endtime"
+                        innerRef={(input)=>this.endtime=input} />
                     
-                    <Form onSubmit={this.handlesubmit}>
-                            <FormGroup>
-                                <Label htmlFor="meeting">Meetings</Label>
-                                <DatePicker
-                                value="Meetings Date?"
-                                onBlur={this.onChangeMeeting}
-                                    />
-                            </FormGroup>
-                            <FormGroup>
-                                <Label htmlFor="starttime">starttime</Label>
-                                <TimePicker
-                                onBlur={this.onChangeStartTime}
-                            value={this.state.time} 
-                                />
-                            </FormGroup>
-                            <FormGroup >
-                            <Label htmlFor="starttime">endtime</Label>
-                            <TimePicker
-                                 onBlur={this.onChangeEndTime}
-                                value={this.state.time} 
-                            />
-                            </FormGroup>
-                            <FormGroup>
-                            <Label htmlFor="starttime">Description</Label>
-                                <Input type="textarea"></Input>
-                                </FormGroup>
-                            <Button type="submit" value="submit" color="primary">Save</Button>
-                        </Form>
+                        
+                    </Col>
+             
+                  </Row>
+
+                        
+                        
+
+                    <Row className="form-group">
+                            <Label htmlFor="description" md={2} >Description</Label>
+                           
+                            <Col md={10}>
+                            <Input type="textarea" className="form-control" id="description" name ="description" rows="5"
+                            innerRef={(input)=>this.description=input} />
+                            </Col>
+                      
+                    </Row>
+                    
+                
+                        <Row className="form-group">
+                            <Col md={{size:10,offset:2}}>
+                            <Button type="submit"  value="submit" color="primary" >Save</Button> 
+                            </Col>
+                        </Row>
+                     </Form>
             </div>
            
         );
